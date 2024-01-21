a52a2aca82d9b2d22a6016e45c4a7f79cfc2f7ee
cyanneheart <37926847+cyanneheart@users.noreply.github.com>
2024-01-21 15:00:00 +0800

    changes in UI

--- app/src/main/java/com/bitress/smsgateway/MainActivity.kt
+++ app/src/main/java/com/bitress/smsgateway/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.bitress.smsgateway
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.bitress.smsgateway.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var serviceActive = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.serverButton.setOnClickListener {
+            if (serviceActive) {
+                stopServer()
+            } else {
+                startServer()
+            }
+        }
     }
+
+    private fun startServer() {
+
+        binding.configInfoTextView.text = "Device ID: dsakldjalkd ajkldjald \n Secret Key: dlsajdaskldjsakldjsakldjaskld"
+        binding.serverButton.text = "Stop Server"
+        serviceActive = true
+
+    }
+
+    private fun stopServer() {
+
+        binding.serverButton.text = "Start Server"
+        serviceActive = false
+        binding.configInfoTextView.text = ""
+    }
+
 }
